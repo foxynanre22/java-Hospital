f581721a408ad4787ebb82e66b5b421a60e05d0f
jew228ill <51099214+jew228ill@users.noreply.github.com>
2021-07-08 18:37:47 +0200

    Add Security

---
+++ src/main/java/pl/wsiz/iid6/gr2/patientservice/configuration/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package pl.wsiz.iid6.gr2.patientservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        String[] staticResources  =  {
+                "/css/**",
+                "/images/**",
+                "/fonts/**",
+                "/scripts/**",
+        };
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/resources/**").permitAll()
+                .antMatchers(staticResources).permitAll()
+                .antMatchers("/leki/**", "/api/**").hasRole("USER")
+                .antMatchers("/h2").hasRole("ADMIN")
+                .anyRequest().authenticated() .and() .formLogin()
+                .loginPage("/login") .permitAll() .and() .logout()
+                .logoutUrl("/logout");
+    }
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("AdamSokol")
+                .password("haslo123")
+                .roles("USER") .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN") .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+}
